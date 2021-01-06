4addd1048191e55b0efbe52094d93c35bb38f91e
MarkLagodych <lgd.mrk@gmail.com>
2021-01-06 14:02:53 +0200

    modified:   bin/server_voter/uploader.py deleted:    doc/how_to_compile.txt deleted:    doc/how_to_use.txt

--- bin/server_voter/uploader.py
+++ bin/server_voter/uploader.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import io
 
 try:
     import serial
@@ -46,18 +47,86 @@ def upload(firmware_file):
         firmware_file
     ])
 
+settings = {
+    'all':         'a',
+    
+    'ssid':        's',
+    'password':    'p',
+    
+    'ip':          'i',
+    'full_ip':     'f',
+    'subnet':      'b',
+    'gateway':     'g',
+    
+    'server_ip':   'v',
+    'server_port': 'o',
+    'AP_mode':     'm', # Access Point mode (autonomic)
+    
+    'type':        't'
+}
+
+def translateSetting(s):
+    keys = list(settings.keys())
+    assert keys.count(s) != 0, "'%s' is not a setting!" % s
+    return bytes(settings[s], encoding = 'ASCII')
+
+ser = serial.Serial()
+
+def queryBegin(freq = 115200):
+    if ser.is_open():
+        ser.close()
+    ser.baudrate = freq
+    ser.port = port
+    ser.open()
+    ser.write(b'q')        # Query mode
+    print(ser.readline())
+
+def queryEnd():
+    if ser.is_open():
+        ser.write(b'e')    # End
+        ser.close()
+
+def queryRaw(q):
+    if not ser.is_open():
+        print('Serial port %s is not opened!' % port)
+        return None
+    ser.write(q)
+    return ser.readline()
 
 def query(setting = 'all'):
     '''
     Query the setting (or all of them) of a connected device
     '''
+        
+    query = b'g' + translateSetting(setting) # Get
+    print(queryRaw(query))
+
+def querySet(setting, value):
+    '''
+    Set the setting (or all of them) of a connected device
+    '''
+
+    if type(value) != bytes:
+        if type(value) == str:
+            value = bytes(value, encoding = 'ASCII')
+        elif type(value) in [int, bool]:
+            value = bytes([value])
+        else:
+            value = bytes(value)
+            
+    query = b's' + translateSetting(setting) + value # Set
+    print(queryRaw(query))
+
+def queryReset(setting = 'all'):
+    '''
+    Reset the setting (or all of them) of a connected device
+    '''
     ser = serial.Serial(port, 115200)
     if not ser.is_open:
         ser.open()
-    ser.write(b'g')
-    respond = ser.readline()
-    print(respond)
-    ser.close()
+
+    query = b'r' + translateSetting(setting) # Reset
+    print(queryRaw(query))
     
 
 def devices():
