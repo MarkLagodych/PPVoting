5779ecef2438e730fda2f38f3e0462b4ad187e5f
MarkLagodych <lgd.mrk@gmail.com>
2021-01-13 16:32:49 +0200

    new file:   bin/server_voter/server.bin modified:   bin/server_voter/uploader.py modified:   src/server/server.ino

--- bin/server_voter/uploader.py
+++ bin/server_voter/uploader.py
@@ -48,19 +48,17 @@ def upload(firmware_file):
         firmware_file
     ])
 
-settings = {
-    'all':         'a',
-    
+
+
+settings_dict = {
     'ssid':        's',
     'password':    'p',
     
     'ip':          'i',
-    'full_ip':     'I',
     'subnet':      'n',
     'gateway':     'g',
     
-    'server_ip':   'v',
-    'AP_mode':     'm', # Access Point mode (autonomic)
+    'server_ip':   'I',
     
     'type':        't'
 }
@@ -68,40 +66,43 @@ settings = {
 def translateSetting(s):
     keys = list(settings.keys())
     assert keys.count(s) != 0, "'%s' is not a setting!" % s
-    return bytes(settings[s], encoding = 'ASCII')
+    return bytes(settings_dict[s], encoding = 'ASCII')
 
 ser = serial.Serial()
 
-def queryBegin(freq = 115200):
+def beginSettings(freq = 115200):
     if ser.is_open():
         ser.close()
     ser.baudrate = freq
     ser.port = port
     ser.open()
-    ser.write(b'q')        # Query mode
+    start = ser.readline()
+    assert start == "?\n", "Failed to enter setting mode, incorrect respond: %s" % str(start) 
+    ser.write(b'!')
     print(ser.readline())
-
-def queryEnd():
+        
+def endSettings():
     if ser.is_open():
         ser.write(b'e')    # End
         ser.close()
 
-def queryRaw(q):
+def query(q):
     if not ser.is_open():
         print('Serial port %s is not opened!' % port)
         return None
     ser.write(q)
-    return ser.readline()
+    if ser.in_waiting():
+        return ser.readline()
+    else
+        return '{No response given}'
 
-def query(setting = 'all'):
+def getSettings():
     '''
-    Query the setting (or all of them) of a connected device
+    Query all the settings of a connected device
     '''
-        
-    query = b'g' + translateSetting(setting) # Get
-    print(queryRaw(query))
+    print(query(b'g')) # Get
 
-def querySet(setting, value):
+def setSetting(setting, value):
     '''
     Set the setting (or all of them) of a connected device
     '''
@@ -115,17 +116,6 @@ def querySet(setting, value):
             value = bytes(value)
             
     query = b's' + translateSetting(setting) + value # Set
-    print(queryRaw(query))
-
-def queryReset(setting = 'all'):
-    '''
-    Reset the setting (or all of them) of a connected device
-    '''
-    ser = serial.Serial(port, 115200)
-    if not ser.is_open:
-        ser.open()
-
-    query = b'r' + translateSetting(setting) # Reset
     print(queryRaw(query))
     
 
--- src/server/server.ino
+++ src/server/server.ino
@@ -4,167 +4,177 @@
  *                                                                     *
  * - Эта программа должна запускаться на ESP8266.                      *
  *   Задачи программы:                                                 *
+ *       0. хранить настройки в (эмулированной) EEPROM;                *
  *       1. получать данные от пультов слушателей по WiFi;             *
  *       2. передавать данные ПК докладчика.                           *
- *       3* интерактивно настраиваться по последовательному порту      *
  *                                                                     *
  * - Передача данных ПК возможна при помощи драйвера CH341SER.         *
  *   Его можно скачать с оф. сайта разработчика:                       *
  *       http://www.wch.cn/downloads/CH341SER_EXE.html                 *
  *   или взять из папки PPVoting/bin/driver                            *
  *                                                                     *
- * - Сеть WiFi может быть создана самим ESP8266 или любым другим       * 
- *   устройством. В первом случае макс. кол-во пультов - 8.            *
+ * - Сеть WiFi может быть создана самим ESP8266 (макс. 8 пультов) или  *
+ * любым другим устройством.                                           *
  * ------------------------------------------------------------------- *
  *                                                                     *
  *                            КАК РАБОТАЕТ КОД                         *
  * ------------------------------------------------------------------- *
  *                           * Общение по WiFi *                       *
+ * 0. Ожидание настроек: отослать "?" и ждать. Если пришло "!", войти  *
+ *    в режим настройки, если нет, продолжить работу                   *
  * 1. Поключиться к / Создать сеть WiFi.                               *
  * 2. Создать веб-сервер.                                              *
  * 3. При каждом запросе веб-страницы вида /Х, где Х - число от 0 до 6 * 
  *    записать голос за Х-й вариант.                                   *
- *                                                                     *
- *                           * Общение по USB *                        *
- * 1. Начать общение по USB.                                           *
- * 2. При каждом запросе "c" очистить текущие результаты голосования.  *
- * 3. При каждом запросе "g" выдать JSON-строку с общим результатом    *
- *    голосования и подробным.                                         *
- *    Общий результат - это список вида                                *
- *        [N0, N1, ..., N6], где Ni - кол-во голосов за i-й вариант.   *
- *    Подробный результат - это список пар вида                        *
- *        "IP-адрес", "Выбранный вариант".                             *
- * 4* При "q" - входить, при "e" - выходить из режима настройки        *
- *    Следующие за "q" команды:                                        *
- *    gX        -- (get) получить настройку Х                          *
- *    sXABCD... -- (set) изменить настройку Х на ABCD...               *
- *    rX        -- (reset) установить значение по умолчанию            *
+ * 4. Если по последовательному порту пришло "c", очистить все ответы, * 
+ *    а если "g", то выслать JSON-строку с результатом                 *
  *                                                                     *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <map>
+#include <cstring>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-
+#include <EEPROM.h>
 
 // ======================== Настройки сети ========================
 
-#define WIFI_MODE_OWN      1             // Собственная WiFi-сеть
-#define WIFI_MODE_EXTERNAL 2             // Внешняя WiFi-сеть
-#define WIFI_MODE          WIFI_MODE_OWN // Режим WiFi
+enum DeviceType {
+    VoterDevice,
+    ServerDevice,
+    ServerAPDevice // Access Point
+};
 
-const String
-  ssid     = "ArbUZ361",    // Название сети
-  password = "gumANOId1";   // Пароль
-  
-const IPAddress
-  ip(192, 168, 1, 1),       // IP-адрес этого устройства в сети
-  gateway(192, 168, 1, 1),  // Шлюз
-  subnet(255, 255, 255, 0); // Маска подсети
+class Settings {
+public: 
+    byte type;
+    
+    char ssid[32];
+    char password[64];
+    
+    byte ip[4];
+    byte subnet[4];
+    byte gateway[4];
+    byte server_ip[4];
 
-const int
-  webServerPort = 80;       // Порт веб-сервера
+    void start();
+    void settingsMode();
+    void load();
+    void save();
 
-ESP8266WebServer server(webServerPort);
+    void printArray(byte *arr);             // Для IP, шлюза и маски -- только 4 элемента
+    
+} settings;
 
-// Функция ответа клиенту (изменения целесообразны для отладки)
+
+// ======================== Веб-общением ========================
+
+ESP8266WebServer server;
+
+// Функция ответа клиенту
 void respond() {
-  server.send(200, "text/html", "<h1 align=\"center\">Success</h1>");
+    server.send(200, "text/html", "<h1 align=\"center\">Success :D</h1>");
 }
 
+// ================ Процессом голосования =================
 
-// ================ Связанное с процессом голосования =================
+class Voting {
+    int total[7];                // Общий результат (кол-во голосов за каждый вариант)
+    std::map<int, int> detailed; // Подробный (IP - вариант ответа)
+    // Не путать класс std::map со стандартной Ардуиновской функцией map() 
 
-int result_total[7];                // Общий результат
-std::map<int, int> result_detailed; // Подробный результат
-// Не путать класс std::map со стандартной Ардуиновской функцией map() 
+public:
+    void update(int variant);
+    void send();
+    void clear();
+} voting;
 
-void updateResult(int variant);
-void sendResult();
-void clearResult();
-
-
-// ========================== Основной код ==========================
+// ===================================================================
 
 void setup() {
-  
-  /* Модуль ESP8266 для перепрошивки требует подачи тока на специальный
-   * контакт при включении. Если модуль идёт вместе с USB-адаптером,
-   * данное действие пропускается. Однако после запуска/подключения
-   * WiFi-сети ESP8266 не может прошиться без замыкания. Задержка 
-   * при запуске режает проблему.
-   */
-  delay(7000);
-  
-  Serial.begin(115200, SERIAL_8N1); /* 8-N-1 - стандартаная конфигурация
-                                     * последовательного порта
-                                     */
 
-#if WIFI_MODE == WIFI_MODE_OWN
-    WiFi.mode(WIFI_AP);                   // AP - Access Point
-    WiFi.softAPConfig(ip, gateway, subnet); 
-    WiFi.softAP(ssid, password, 1, 0, 8); /* 1, 0 - значения по умолчанию
-                                           * 8 - максимальное кол-во подключённых
-                                           * к локальной сети
-                                           */
-#else
-    WiFi.mode(WIFI_STA);                  // STA - STAtion
-    WiFi.config(ip, gateway, subnet);
-    WiFi.begin(ssid, password);
-#endif
+    delay(1000);
   
-  server.on("/", respond);
+    Serial.begin(115200, SERIAL_8N1);
 
-  #define F(n) [](){respond(); updateResult(n);}
-  server.on("/0", F(0));
-  server.on("/1", F(1));
-  server.on("/2", F(2));
-  server.on("/3", F(3));
-  server.on("/4", F(4));
-  server.on("/5", F(5));
-  server.on("/6", F(6));
-  #undef F
+    // ----------- Режим настроек -----------
+    Serial.println("?");
+    delay(500);
+    settings.start();
+    
+    if (Serial.available())
+        if (Serial.read() == '!') 
+            settings.settingsMode();
+            
+    settings.load();
 
-  server.begin();
+    if (settings.type == ServerAPDevice) {
+        WiFi.mode(WIFI_AP);                                     // AP - Access Point
+        WiFi.softAPConfig(settings.ip, settings.gateway, settings.subnet); 
+        WiFi.softAP(settings.ssid, settings.password, 1, 0, 8); // 8 - максимальное кол-во подключённых к локальной сети
+    } else {
+        WiFi.mode(WIFI_STA);                                    // STA - STAtion
+        WiFi.config(settings.ip, settings.gateway, settings.subnet);
+        
+        Serial.print("Connecting");
+        WiFi.begin(settings.ssid, settings.password);
+        while (WiFi.status() != WL_CONNECTED)
+            Serial.print(".");
+        Serial.println();
+    }
+  
+    server.on("/", respond);
+
+    #define F(n) [](){respond(); voting.update(n);}
+    server.on("/0", F(0));
+    server.on("/1", F(1));
+    server.on("/2", F(2));
+    server.on("/3", F(3));
+    server.on("/4", F(4));
+    server.on("/5", F(5));
+    server.on("/6", F(6));
+    #undef F
+
+    server.begin();
   
 }
 
 
 void loop() {
   
-  server.handleClient();
+    server.handleClient();
 
-  if (Serial.available()) {
+    if (Serial.available()) {
     
-    switch (Serial.read()) {
+        switch (Serial.read()) {
     
-    case 'c':           // 'c' - clear results
-      clearResult();
-      break;
+        case 'c':           // 'c' - clear results
+            voting.clear();
+            break;
     
-    case 'g':           // 'g' - get results
-      sendResult();
-      break;
+        case 'g':           // 'g' - get results
+            voting.send();
+            break;
       
-    }
+        }
     
-  }
+    }  
   
 }
 
 
 
-// =================== Реализация обмена ответами ==================
+// ========================= Голосование =======================
 
-void updateResult(int variant) {
+void Voting::update(int variant) {
   
-  char ip = server.client().remoteIP()[3]; // Последняя часть IP
+    char ip = server.client().remoteIP()[3]; // Последняя часть IP
 
-  // Если устройство с данным IP ещё не голосовало
-  if (result_detailed.find(ip) == result_detailed.end()) {
-    result_total[variant]++;
-    result_detailed.insert( std::make_pair(ip, variant) );
-  }
+    // Если устройство с данным IP ещё не голосовало
+    if (detailed.find(ip) == detailed.end()) {
+        total[variant]++;
+        detailed.insert( std::make_pair(ip, variant) );
+    }
   
 }
 
@@ -183,37 +193,212 @@ void updateResult(int variant) {
  *    ]
  * }
  */
-void sendResult() {   
+void Voting::send() {   
   
-  Serial.print("{\"total\":[");
+    Serial.print("{\"total\":[");
   
-  for (int i=0; i<7; i++) {
-    if (i) Serial.print(',');
-    Serial.print(result_total[i]);
-  }
+    for (int i=0; i<7; i++) {
+        if (i) Serial.print(',');
+        Serial.print(total[i]);
+    }
   
-  Serial.print("],\"votes\":[");
+    Serial.print("],\"votes\":[");
   
-  for (auto i = result_detailed.begin(); i != result_detailed.end(); i++) {
-    if(i != result_detailed.begin()) 
-      Serial.print(',');
+    for (auto i = detailed.begin(); i != detailed.end(); i++) {
+        if(i != detailed.begin()) 
+            Serial.print(',');
       
-    Serial.print('[');
-    Serial.print(i->first);
-    Serial.print(',');
-    Serial.print(i->second);
-    Serial.print(']');
-  }
+        Serial.print('[');
+        Serial.print(i->first);
+        Serial.print(',');
+        Serial.print(i->second);
+        Serial.print(']');
+    }
   
-  Serial.println("]}"); // -ln -- конец строки
+    Serial.println("]}"); // -ln -- конец строки
   
 }
 
-void clearResult() {
+void Voting::clear() {
    
-  for(int i=0; i<7; i++) 
-    result_total[i] = 0; 
+    for(int i=0; i<7; i++) 
+        total[i] = 0; 
     
-  result_detailed.clear();
+    detailed.clear();
   
+}
+
+// ======================== Настройки ======================
+
+void Settings::start() {
+    EEPROM.begin(512);
+}
+
+
+void Settings::printArray(byte *arr) {
+    for (int i=0; i<4; i++) {
+        Serial.print(arr[i], DEC);
+        Serial.print('.');
+    }
+    Serial.println();
+}
+
+void Settings::settingsMode() {
+    
+    while (1) 
+        if (Serial.available()) {
+            
+            char action = Serial.read();
+            
+            switch (action) {
+              
+                case 's': {  // Set
+                    char var = Serial.read();
+                    switch (var) {
+
+                        // Ip
+                        case 'i':
+                            Serial.readBytes(ip, 4);
+                            break;
+
+                        // subNet
+                        case 'n':
+                            Serial.readBytes(subnet, 4);
+                            break;
+
+                        // Gateway
+                        case 'g': 
+                            Serial.readBytes(gateway, 4);
+                            break;
+
+                        // server Ip
+                        case 'I':
+                            Serial.readBytes(server_ip, 4);
+                            break;
+
+                        // Type
+                        case 't':
+                            type = Serial.read();
+                            break;  
+
+                        // Ssid
+                        case 's':
+                            Serial.readBytesUntil(0, ssid, 31);
+                            ssid[31] = 0;
+                            break;
+
+                        // Password
+                        case 'p':
+                            Serial.readBytesUntil(0, password, 63);
+                            password[63] = 0;
+                            break;
+                        
+                    } // switch (var)
+                    
+                    break;
+                    
+                } // case 's'  -- action
+
+                case 'w':   // Write
+                    save();
+                    break;
+
+                case 'g':   // Get
+                    Serial.print("Type: ");
+                    Serial.println(type, DEC);
+                    
+                    Serial.print("SSID: ");
+                    Serial.println(ssid);
+
+                    Serial.print("Password: ");
+                    Serial.println(password);
+
+                    Serial.print("IP: ");
+                    printArray(ip);
+
+                    Serial.print("Server IP: ");
+                    printArray(ip);
+
+                    Serial.print("Gateway: ");
+                    printArray(gateway);
+
+                    Serial.print("Subnet: ");
+                    printArray(subnet);
+                    break;
+
+                case 'r':   // Read
+                    load();
+                    break;
+
+                case 'e':  // End
+                    return;
+
+                default: {
+                    Serial.print("Unknown action: ");
+                    Serial.println(action);
+                    break;
+                }
+            } // switch (action)
+            
+        } // if (Serial.available())
+}
+
+void Settings::load() {
+    int addr = 0;
+
+#define NEXT EEPROM.read(addr++)
+    
+    type = NEXT;
+
+    for (int i=0; i<4; i++)
+        ip[i] = NEXT;
+
+    for (int i=0; i<4; i++)
+        subnet[i] = NEXT;
+
+    for (int i=0; i<4; i++)
+        gateway[i] = NEXT;
+
+  for (int i=0; i<4; i++)
+        server_ip[i] = NEXT;
+    
+    for (int i=0; i<31; i++) 
+        ssid[i] = NEXT;
+    ssid[31] = 0;
+
+    for (int i=0; i<63; i++)
+        password[i] = NEXT;
+    password[63] = 0;
+
+#undef NEXT
+    
+}
+
+void Settings::save() {
+    int addr = 0;
+
+#define NEXT(x) EEPROM.write(addr++, (x));
+    
+    NEXT(type);
+
+    for (int i=0; i<4; i++)
+        NEXT(ip[i]);
+
+    for (int i=0; i<4; i++)
+        NEXT(subnet[i]);
+
+    for (int i=0; i<4; i++)
+        NEXT(gateway[i]);
+
+    for (int i=0; i<4; i++)
+        NEXT(server_ip[i]);
+
+    for (int i=0; i<31; i++)
+        NEXT(ssid[i]);    
+
+    for (int i=0; i<63; i++)
+        NEXT(password[i]);
+
+#undef NEXT
+
 }
