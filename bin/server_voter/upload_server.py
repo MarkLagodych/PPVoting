043876f4d2ff63ea6f652819cf48af58281f194b
MarkLagodych <lgd.mrk@gmail.com>
2021-01-14 00:52:53 +0200

    modified:   bin/server_voter/upload_server.py

--- bin/server_voter/upload_server.py
+++ bin/server_voter/upload_server.py
@@ -19,7 +19,7 @@ setSetting('ip',        [192, 168, 1, 1])
 setSetting('subnet',    [255, 255, 255, 0])
 setSetting('gateway',   [192, 168, 1, 1])
 setSetting('server_ip', [192, 168, 1, 1])
-setSetting('type',      3)
+setSetting('type',      TYPE_SERVER_AP)
 writeSettings()
 
 print('Current settings:')
